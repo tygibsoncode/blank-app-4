68b9706aea82c79ba4cee46a5edef570a3924693
tygibsoncode <tymichaelgibson@gmail.com>
2026-01-29 14:17:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Sign Up")
+
+def convertFtoC(temp):
+    return (temp - 32) * 5/9
+
+def convertCtoF(temp):
+    return temp * 9/5 + 32
+
+temp_input = st.slider("Select the temperature value",
+                       min_value =-100,
+                       max_value=200,
+                       value=32)
+conv_type = st.radio("Select conversion type:",options=
+                     ["Fahrenheit to celsius"
+                      "Celsius to Fahrenheit"])
+
+if "history" not in st.session_state:
+    st.session_state["history"] = []
+
+    st.session_state.history.append((temp_input, conv_type))
+    print(st.session_state.history)
+
+    st.divider()
+    history_data = st.session_state.history
+    
