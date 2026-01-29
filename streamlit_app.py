d2ee0363e249e128bf2e33fc7e80b74e778748da
tygibsoncode <tymichaelgibson@gmail.com>
2026-01-29 14:22:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,14 @@ conv_type = st.radio("Select conversion type:",options=
                      ["Fahrenheit to celsius"
                       "Celsius to Fahrenheit"])
 
+if conv_type == "Fahrenheit to Celsius":
+    output = convertFtoC(temp_input)
+    st.metric(label="Converted Temperature", value=output)
+
+    elif conv_type == "Celsius to Fahrenheit":
+    output = convertCtoF(temp_input)
+    st.metric(label="Converted Temperature", value=output)
+
 if "history" not in st.session_state:
     st.session_state["history"] = []
 
@@ -24,4 +32,10 @@ if "history" not in st.session_state:
 
     st.divider()
     history_data = st.session_state.history
-    
+
+    rev = list(reversed(st.session_state.history[-5:]))
+
+    st.subheader("Recent conversions:")
+
+    for i, (temp,conv, output) in enumerate(rev,1):
+        st.write(f"{i}, {temp} ({conv})->{output}")
